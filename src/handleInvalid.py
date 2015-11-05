088d7d894eac9200b96fac658e927a587376f97f
Jesse <jesse.xu.20@gmail.com>
2015-11-05 15:27:55 -0500

    add handle invalid script

---
+++ src/handleInvalid.py
@@ -0,0 +1,15 @@
+def handle(inputFile,outputFile):
+     fd = open(inputFile, 'r')
+     wd = open(outputFile, 'w')
+     wd.write(fd.readline())
+     for line in fd:
+         arr = line.split(',')
+         if(len(arr)<8):
+             continue
+         if(arr[8]!="" and arr[8].find("male")!=-1):
+             wd.write(line+"\n")
+     fd.closed
+if __name__ == '__main__':
+    import sys
+    handle(sys.argv[1],sys.argv[2])
+
